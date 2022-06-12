9563dacee3d44fca2619ac12da0db549dcde6bb7
swiniozaur <swiecki@onet.eu>
2022-06-12 13:53:54 +0200

    test

---
+++ src/main/java/pages/user/DetailedOrderDetailsPage.java
@@ -0,0 +1,14 @@
+package pages.user;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import pages.base.BasePage;
+
+public class DetailedOrderDetailsPage extends BasePage {
+
+    public DetailedOrderDetailsPage(WebDriver driver) {
+        super(driver);
+    }
+
+}
